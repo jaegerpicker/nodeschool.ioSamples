a880a5357fa5860c9cc975798ff16876771c9c5f
Shawn Campbell <scampbell@cashstar.com>
2014-01-13 11:57:23 -0500

    Level up work

--- levelmeup/basics_get.js
+++ levelmeup/basics_get.js
@@ -1,2 +1,19 @@
 var level = require('level');
 var db = level(process.argv[2]);
+function fetchNext(i) {
+    //console.log(i);
+    var key = 'gibberish' + i;
+    db.get(key, function(err, value) {
+        if(!err){
+            console.log(key + '=' + value);
+        } else {
+            if(!err.notFound) {
+                throw err;
+            }
+        }
+        if(i < 100) {
+            fetchNext(i + 1);
+        }
+    });
+}
+fetchNext(0);
